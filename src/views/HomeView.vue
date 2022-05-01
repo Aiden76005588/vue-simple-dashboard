0b80782a3be6ddd4c544b8d4a244479b827d27be
Aiden <fsd76005588@gmail.com>
2022-05-01 19:26:31 +0900

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import apiBoard from "@/api/board";
+
+export default {
+  name: "HomeView",
+  mounted() {
+    apiBoard
+      .getArticle(1)
+      .then((response) => {
+        console.log("getArticles", response);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+
+    apiBoard
+      .getArticles(0)
+      .then((response) => {
+        console.log("getArticles", response);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+
+    apiBoard
+      .postArticle("id", "title", "body")
+      .then((response) => {
+        console.log("postArticles", response);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  },
+  components: {
+    HelloWorld,
+  },
+};
+</script>
